7a57a4b540d4edf8ad9d2699bb63ba0e47e6e34f
Priyanshu1213 <priyanshuyadav39976@gmail.com>
2024-05-11 02:20:59 +0530

    6th production

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app=express();
 const bodyParser = require("body-parser")
 const db= require("./db")
-const cors= requre("cors")
+const cors= require("cors")
 
 
 
